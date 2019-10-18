d1b9d0717fee92bdf39a9db2caaa2ce7d062bf7d
iansee <37576330+iansee@users.noreply.github.com>
2019-10-18 13:04:27 +0800

    Update mininet.py

--- WorkingExamples/mininet.py
+++ WorkingExamples/mininet.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
-import sys
-sys.path.append('/usr/local/lib/python2.7/dist-packages/mininet-2.2.2-py2.7.egg')
+
+import threading
+from multiprocessing import Queue
+import time
+import random
 from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.node import CPULimitedHost
@@ -15,20 +18,37 @@ class SingleSwitchTopo( Topo ):
         switch = self.addSwitch('s1')
         for h in range(n):
             host = self.addHost('h%s' % (h + 1))
-            if n == 4:
-                # 10 Mbps, 5ms delay, 2% loss
-                self.addLink( host, switch, bw=10, delay='5ms',loss=2)
-            else:
-                self.addLink( host, switch, bw=100)
+            a = self.addLink( host, switch)
+            print ('Added host %s' % (h + 1))
+
 
 def perfTest():
+    q = Queue()
     "Create network and run simple performance test"
     topo = SingleSwitchTopo( n=4 )
     net = Mininet( topo=topo, link=TCLink )
     net.start()
-    CLI(net)
-    
-    
+    commandprompt = threading.Thread(target=start_CLI, args=(net,))
+    dynamiclinking = threading.Thread(target=linkfunc, args=(q,net,))
+    commandprompt.start()
+    dynamiclinking.start()
+    commandprompt.join()
+    q.put_nowait('1')
+    dynamiclinking.join()
+    net.stop()
 
+
+def start_CLI(network):
+    CLI(network)
+
+def linkfunc(q,net):
+    h1,h2,h3,h4 = net.get('h1','h2','h3','h4')
+    while q.empty():
+        time.sleep(30)
+        #set randomized parameters here
+        h4.intf().config(bw=1000,loss=20)
+    return
+
+    
 if __name__ == '__main__':
     perfTest()
