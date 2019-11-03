c4a85b0d58f42f4933a50573e5fd6558439f05bd
noirag <yuxuanng@hotmail.com>
2019-11-04 01:12:21 +0800

    introduced loss between switches

--- WorkingExamples/Federated_Swarm.py
+++ WorkingExamples/Federated_Swarm.py
@@ -1,4 +1,9 @@
-from syft.workers import WebsocketServerWorker
+import tensorflow as tf
+import h5py
+import collections
+import six
+
+from syft.workers.websocket_server import WebsocketServerWorker
 import torch
 import sys
 import syft
@@ -8,34 +13,62 @@ from torchvision import datasets
 from torchvision import transforms
 import numpy as np
 
+import tensorflow_federated as tff
+import os.path
+from tensorflow_federated.python.simulation.hdf5_client_data import HDF5ClientData
+
+fractionToUse = 5
+
+class TrainDataset:
+    def __init__(self,transform=None, number=2):
+        fileprefix = "fed_emnist_digitsonly"
+        dir_path = os.path.dirname("/home/mininet/")
+        train = HDF5ClientData(os.path.join(dir_path, fileprefix + '_train.h5'))
+        trainFile = h5py.File(os.path.join(dir_path, fileprefix + '_train.h5'), "r")
+        _EXAMPLES_GROUP = "examples"
+        numberofclients = len(train.client_ids)
+        data = np.empty((0,28,28), np.float32)
+        target = np.empty((0), np.int_)
+        offset = int(number) - 1
+        for i in range(int(numberofclients/(5*fractionToUse))):
+            clientdataset = collections.OrderedDict((name, ds[()]) for name, ds in sorted(
+                six.iteritems(trainFile[HDF5ClientData._EXAMPLES_GROUP][train.client_ids[i*5*fractionToUse+offset]])))
+            data = np.concatenate((data, clientdataset['pixels']))
+            target = np.concatenate((target, clientdataset['label']), axis=0)
+        self.target = list(target)
+        self.data = list(data)
+        self.transform = transform
+        trainFile.close()
+        del train
+
+    def __getitem__(self, index):
+        x=self.data[index]
+        y=self.target[index]
+        if self.transform:
+            x = self.transform(x)
+        return x,y
+    def __len__(self):
+        return len(self.target)
 
 def main(number):
-    mnist_dataset = datasets.MNIST(
-        root="./files/",
-        train=True,
-        download=False,
-        transform=transforms.Compose(
-            [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
-        ),
-    )
-    selected_data = mnist_dataset.data
-    selected_targets = mnist_dataset.targets
+    mnist_dataset = TrainDataset(transform=transforms.Compose([
+      transforms.ToTensor(),
+      transforms.Normalize(
+        (0.1307,), (0.3081,))
+        ]), number=number)
     _id = 'h%s'%number
     ip  = '10.0.0.%s'%number
 
     hook = syft.TorchHook(torch)
-    
+
     server = WebsocketServerWorker(id = _id,host =ip, port = 8778,hook=hook,verbose=True)
-    
-    print ("Worker:{}, Dataset contains {}".format(_id,str(len(selected_data))))
+    print ("Worker:{}, Dataset contains {}".format(_id,str(len(mnist_dataset.data))))
     dataset = syft.BaseDataset(
-    data=selected_data, targets=selected_targets, transform=mnist_dataset.transform
+    data=mnist_dataset.data, targets=mnist_dataset.target, transform=mnist_dataset.transform
         )
     key = "targeted"
     server.add_dataset(dataset, key=key)
     server.start()
-    
+
 
 main (sys.argv[1])
-
-    
--- WorkingExamples/mininet_swarm.py
+++ WorkingExamples/mininet_swarm.py
@@ -16,24 +16,46 @@ from mininet.cli import CLI
 
 class SingleSwitchTopo( Topo ):
     "Single switch connected to n hosts."
-    def build( self, n=0 ):
+    def build( self, nodes = 0, bad_nodes = 0, upperbound = 0, lowerbound = 0):
         switch = self.addSwitch('s1')
-        for h in range(n):
-            host = self.addHost('h%s' % (h + 1))
-            a = self.addLink( host, switch)
-            print ('Added host %s' % (h + 1))
+        host = self.addHost('h1')
+        a = self.addLink( host, switch)
+        print ('Added host h1')
 
-    
+        counter = 0
+        bad_node_list = []
+        while (counter != bad_nodes):
+            bad_host = random.randint(1,nodes)
+            if bad_host not in bad_node_list:
+                bad_node_list.append(bad_host)
+                counter+=1
+
+        for h in range(1, nodes):
+            switch1 = self.addSwitch('s%s' % (h + 1))
+            host = self.addHost('h%s' % (h + 1))
+            if h in bad_node_list:
+                random_loss = random.randint(lowerbound,upperbound)
+                print ("h{} has been choosen as a faulty host with a loss of {}".format(
+                    h+1,random_loss))
+                a = self.addLink( switch, switch1, loss=random_loss)
+            else:
+                print ("h{} has been choosen as a good host".format(
+                    h+1))
+                a = self.addLink( switch, switch1)
+            a = self.addLink( host, switch1)
+            print ('Added host h%s' % (h + 1))
+
+
 def runcommand(number,network):
     name = 'h%s'%number
     host = network.get(name)
     print ('Created Federated Client on Host {}'.format(name))
-    command = ('python /home/mininet/imported_files/Federated_Swarm.py %s' %(number))
+    command = ('python /home/mininet/Federated_Swarm.py %s' %(number))
     host.sendCmd(command)
 
 def start_CLI(network):
     CLI(network)
-    
+
 
 def linkfunc(net,upperbound,lowerbound,bad_nodes,nodes):
     counter = 0
@@ -44,7 +66,7 @@ def linkfunc(net,upperbound,lowerbound,bad_nodes,nodes):
         if bad_host not in bad_node_list:
             bad_node_list.append(bad_host)
             counter+=1
-    
+
     for bad_host in bad_node_list:
         this_bad_host = net.get(bad_host)
         random_loss = random.randint(lowerbound,upperbound)
@@ -53,30 +75,30 @@ def linkfunc(net,upperbound,lowerbound,bad_nodes,nodes):
 
         this_bad_host.intf().config(loss=random_loss)
 
-            
+
 def main():
     print ("Use H1 as the centralized node")
     nodes = int(input("Enter amount of nodes:"))
     bad_nodes = int(input("Enter amount of nodes with faulty connection:"))
     upperbound = int(input("Enter the upper bound for loss:"))
     lowerbound = int(input("Enter the lower bound for loss:"))
-    
-    topo = SingleSwitchTopo(nodes)
+
+    topo = SingleSwitchTopo(nodes, bad_nodes, upperbound, lowerbound)
     net = Mininet( topo=topo, link=TCLink )
     net.start()
     commandprompt = threading.Thread(target=start_CLI, args=(net,))
     commandprompt.start()
     time.sleep(5)
     print ('\n')
-    
-    print ('Starting varying packet loss')
-    linkfunc(net,upperbound,lowerbound,bad_nodes,nodes)
-    
+
+    # print ('Starting varying packet loss')
+    # linkfunc(net,upperbound,lowerbound,bad_nodes,nodes)
+
     for i in range(2,nodes+1):
         runcommand(i,net)
-        
+
 
     commandprompt.join()
-            
+
 if __name__ == '__main__':
     main()
