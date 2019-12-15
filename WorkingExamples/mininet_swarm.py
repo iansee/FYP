3ce5b377b2d41f7cdc19a4dc4cfbdde1a066e36e
iansee <37576330+iansee@users.noreply.github.com>
2019-12-15 14:56:49 +0800

    Update mininet_swarm.py
    Added link. Both interfaces now are configured with random loss

--- WorkingExamples/mininet_swarm.py
+++ WorkingExamples/mininet_swarm.py
@@ -16,45 +16,25 @@ from mininet.cli import CLI
 
 class SingleSwitchTopo( Topo ):
     "Single switch connected to n hosts."
-    def build( self, nodes = 0, bad_nodes = 0, upperbound = 0, lowerbound = 0):
+    def build( self, n=0 ):
         switch = self.addSwitch('s1')
-        host = self.addHost('h1')
-        a = self.addLink( host, switch)
-        print ('Added host h1')
-
-        counter = 0
-        bad_node_list = []
-        while (counter != bad_nodes):
-            bad_host = random.randint(1,nodes)
-            if bad_host not in bad_node_list:
-                bad_node_list.append(bad_host)
-                counter+=1
-
-        for h in range(1, nodes):
-            switch1 = self.addSwitch('s%s' % (h + 1))
+        for h in range(n):
             host = self.addHost('h%s' % (h + 1))
-            if h in bad_node_list:
-                random_loss = random.randint(lowerbound,upperbound)
-                print ("h{} has been choosen as a faulty host with a loss of {}".format(
-                    h+1,random_loss))
-                a = self.addLink( switch, switch1, loss=random_loss)
-            else:
-                print ("h{} has been choosen as a good host".format(
-                    h+1))
-                a = self.addLink( switch, switch1)
-            a = self.addLink( host, switch1)
-            print ('Added host h%s' % (h + 1))
+            a = self.addLink( host, switch)
+            print ('Added host %s' % (h + 1))
 
-
+    
 def runcommand(number,network):
     name = 'h%s'%number
     host = network.get(name)
     print ('Created Federated Client on Host {}'.format(name))
-    command = ('python /home/mininet/Federated_Swarm.py %s' %(number))
+    command = ('python /home/mininet/imported_files/Federated_Swarm.py %s' %(number))
     host.sendCmd(command)
 
 def start_CLI(network):
     CLI(network)
+
+
 
 
 def linkfunc(net,upperbound,lowerbound,bad_nodes,nodes):
@@ -66,39 +46,49 @@ def linkfunc(net,upperbound,lowerbound,bad_nodes,nodes):
         if bad_host not in bad_node_list:
             bad_node_list.append(bad_host)
             counter+=1
-
+            
+    switch = net.get('s1')
     for bad_host in bad_node_list:
         this_bad_host = net.get(bad_host)
+        linkslist = net.linksBetween(switch,this_bad_host)
+        thislink = linkslist[0] #Get link which has the 2 interfaces
+        
+
         random_loss = random.randint(lowerbound,upperbound)
         print ("{} has been choosen as a faulty host with a loss of {}".format(
             bad_host,random_loss))
+        
+        this_inf1 = thislink.intf1 #set interface 1
+        this_inf1.config(loss=random_loss)
+        this_inf2 = thislink.intf2 #set interface 2
+        this_inf2.config(loss=random_loss)
 
-        this_bad_host.intf().config(loss=random_loss)
 
 
+            
 def main():
     print ("Use H1 as the centralized node")
     nodes = int(input("Enter amount of nodes:"))
     bad_nodes = int(input("Enter amount of nodes with faulty connection:"))
     upperbound = int(input("Enter the upper bound for loss:"))
     lowerbound = int(input("Enter the lower bound for loss:"))
-
-    topo = SingleSwitchTopo(nodes, bad_nodes, upperbound, lowerbound)
+    
+    topo = SingleSwitchTopo(nodes)
     net = Mininet( topo=topo, link=TCLink )
     net.start()
     commandprompt = threading.Thread(target=start_CLI, args=(net,))
     commandprompt.start()
     time.sleep(5)
     print ('\n')
-
-    # print ('Starting varying packet loss')
-    # linkfunc(net,upperbound,lowerbound,bad_nodes,nodes)
-
+    
+    print ('Starting varying packet loss')
+    linkfunc(net,upperbound,lowerbound,bad_nodes,nodes)
+    
     for i in range(2,nodes+1):
         runcommand(i,net)
-
+        
 
     commandprompt.join()
-
+            
 if __name__ == '__main__':
     main()
